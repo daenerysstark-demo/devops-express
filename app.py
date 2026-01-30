fa5d409451b65bef1868ed19b462de8eb8d642f7
Daenerys Stark <daenerysstark.108249@gmail.com>
2026-01-30 09:28:51 -0800

    app with downloader

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+# Import blueprints
+from features.downloader import downloader_bp
+
+app = Flask(__name__)
+
+# Register Blueprints
+app.register_blueprint(downloader_bp)
+
+# --- Frontend Routes ---
+@app.route('/')
+def index():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    # Debug mode on for extra "developer visibility" (also a risk in prod)
+    app.run(debug=True, port=5000)
