c4c81ad7da1664e1cf11cf9227c902416d1bb963
Daenerys Stark <daenerysstark.108249@gmail.com>
2026-02-11 15:45:28 -0800

    feat: add file uploader

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 # Import blueprints
 from features.downloader import downloader_bp
+from features.task_runner import task_runner_bp
 
 app = Flask(__name__)
 
 # Register Blueprints
 app.register_blueprint(downloader_bp)
+app.register_blueprint(task_runner_bp)
 
 # --- Frontend Routes ---
 @app.route('/')
